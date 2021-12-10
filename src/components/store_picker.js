64fa428ade99bf8c3b0e7a585e148aed0b24c28e
ana sanots <anasoliveiraaa@gmail.com>
2021-12-10 11:02:00 -0500

    ref added to form

--- src/components/store_picker.js
+++ src/components/store_picker.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers'
 
 class StorePicker extends Component {
+    myInput = React.createRef(); // this allows me to the inout value as ref from the form and use it in goToStore function
+
+    goToStore = (e) => {
+        e.preventDefault()
+
+        console.log(this.myInput.current.value)
+
+    }
+
     render() {
         return (
             <>
-                <form action="" className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>Please Enter a Store</h2>
-                    <input type="text" required placeholder="Store Name" />
+                    <input type="text" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()}/>
                     <button type="submit"> Vistit Store </button>
                 </form>
             </>
